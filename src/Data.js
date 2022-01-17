9187601579b6046e50a33c2748e85a51c6d36a56
Anjali2424 <71063093+Anjali2424@users.noreply.github.com>
2022-01-18 00:47:25 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,44 @@
+const Data = [
+    {
+        id : 1,
+        name : "Anjali Markande",
+        email : "anjalimarkande24@gmail.com"
+    },
+    {
+        id : 2,
+        name : "Pragya",
+        email : "psingh@gmail.com"
+    },
+    {
+        id : 3,
+        name : "Kartik",
+        email : "kartik4@gmail.com"
+    },
+    {
+        id : 4,
+        name : "Manjusha Mekala",
+        email : "manjushamekala@gmail.com"
+    },
+    {
+        id : 5,
+        name : "Pawan Shah",
+        email : "pawanshah@gmail.com"
+    },
+    {
+        id : 6,
+        name : "Sandeep Shah",
+        email : "sandeepshah@gmail.com"
+    },
+    {
+        id : 7,
+        name : "Amit Shah",
+        email : "amitshah@gmail.com"
+    },
+    {
+        id : 8,
+        name : "Narendra Modi",
+        email : "narendramodi@gmail.com"
+    },
+]
+
+export default Data;
